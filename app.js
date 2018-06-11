ada709558d3111deb5fc197d3ee51b94b3cd903f
Rachel <rachel.ulgado@gmail.com>
2018-06-11 09:49:48 -0700

    cleanup

--- app.js
+++ app.js
@@ -3,11 +3,24 @@ var express = require('express');
 var app     = express();
 var PORT    = 3000;
 
+// node server on port 3000
 var server  = app.listen(PORT);
 var io      = require('socket.io')(server);
 
+// -----------------------------------------------------
+// express app setup
+// -----------------------------------------------------
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
+});
+
+// -----------------------------------------------------
+// osc server setup
+// -----------------------------------------------------
+var oscServer = new osc.Server(8000, '192.168.1.6');
+
+oscServer.on('message', function(msg, rinfo) {
+  console.log('osc got msg: ', msg);
 });
